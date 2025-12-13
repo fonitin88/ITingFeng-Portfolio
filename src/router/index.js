6546ae46eae04f8ed2f5632ae4672b63ff9025f6
fonitin88 <92967999+fonitin88@users.noreply.github.com>
2025-12-13 16:12:08 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 // router/index.js
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 // 匯入您的頁面元件
 import AboutPage from '../views/AboutPage.vue';
 import Experience from '../views/Experience.vue';
